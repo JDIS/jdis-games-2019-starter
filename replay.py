4fd26c7754dd23af9fcccd95be67fbfc30ec8848
Étienne Lévesque <etienne.levesque16@gmail.com>
2019-07-06 15:57:37 -0400

    Fixed replay.py

--- replay.py
+++ replay.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 from pacman.capture import *
 import pacman.captureGraphicsDisplay as captureGraphicsDisplay
-from bots import baselineTeam
 import pacman.layout as layout
 import sys
 
@@ -9,8 +8,8 @@ if __name__ == '__main__':
     replay = sys.argv[1]
     display = captureGraphicsDisplay.PacmanGraphics("Red", "Blue", 1, 0, capture=False)
     import pickle
-    with open(replay, 'rb') as f:
-        recorded = pickle.load(open(replay, 'rb'))
+    with open(f"replays/{replay}", 'rb') as f:
+        recorded = pickle.load(f)
         recorded['display'] = display
         replayGame(**recorded)
     sys.exit(0)
